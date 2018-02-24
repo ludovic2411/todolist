67225f399176249f933e939f2ebe8554a08d1921
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-02-24 12:10:39 +0100

    final

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-//sanitisation
-// function sanitize($key, $filter=FILTER_SANITIZE_STRING){
-//     $sanitized_variable = null;
-//     if(isset($_POST['tache'])OR isset($_POST['boutton'])){
-//         if(is_array($key)){                 // si la valeur est un tableau...
-//         $sanitized_variable = filter_var_array($key, $filter);
-//         }
-//         else {                              // sinon ...
-//         $sanitized_variable = filter_var($key, $filter);
-//         }
-//     }
-//     return $sanitized_variable;
-// }
 $jsonURL="todo.json"; //source
 $jsonReceived = file_get_contents($jsonURL); //prendre le fichier
 $log = json_decode($jsonReceived, true); //décoder ( true = dans un tableau )
+//sanitization
+function sanitize($key, $filter=FILTER_SANITIZE_STRING){
+    $sanitized_variable = null;
+    if(isset($_POST['tache'])OR isset($_POST['boutton'])){
+        if(is_array($key)){                 // si la valeur est un tableau...
+        $sanitized_variable = filter_var_array($key, $filter);
+        }
+        else {                              // sinon ...
+        $sanitized_variable = filter_var($key, $filter);
+        }
+    }
+    return $sanitized_variable;
+}
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \\
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \
 if (isset($_POST['ajouter']) AND end($log)['nomtache'] != $_POST['tache']){ //Si on appuie sur le boutton ajouter...
-$add_tache =$_POST['tache']; //je récupère la valeur que je veux ajouter
+$add_tache =sanitize( $_POST['tache']); //je récupère la valeur que je veux ajouter
 $array_tache = array("nomtache" => $add_tache, // je la met dans un tableau
 "fin" => false);
 $log[] = $array_tache; // je crée un tableau multi dimensionnel
@@ -27,7 +28,7 @@ $log = json_decode($json_enc, true); // je décode le tout pour pouvoir le lire
 
 }
 if (isset($_POST['boutton'])){ //si j'enregistre ( je check la case )
-$choix=$_POST['tache']; // je récupère les valeurs checkée ("tache[]") des inputs ( qui sont alors dans un tableau )
+$choix=sanitize($_POST['tache']); // je récupère les valeurs checkée ("tache[]") des inputs ( qui sont alors dans un tableau )
 
 for ($init = 0; $init < count($log); $init ++){         // Pour chaque ligne du tableau
     if (in_array($log[$init]['nomtache'], $choix)){     // Je compare les valeurs checkée avec le tableau

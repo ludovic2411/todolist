bf70a36bcdfcecd6898e6a8e6879116890c57c20
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-02-28 12:13:50 +0100

    second

--- index.php
+++ index.php
@@ -13,30 +13,32 @@ function sanitize($key, $filter=FILTER_SANITIZE_STRING){
   }
   return $sanitized_variable;
 }
-
+try
+{
+  // On se connecte à MySQL
+  $bd = new PDO('mysql:host=localhost;dbname=todo_list;charset=utf8', 'root', 'user');
+}
+catch(Exception $e)
+{
+  // En cas d'erreur, on affiche un message et on arrête tout
+  die('Erreur : '.$e->getMessage());
+}
 
 if (isset($_POST['ajouter'])  and !empty ($_POST['tache'])){ //Si on appuie sur le boutton ajouter...
   $add_tache =sanitize( $_POST['tache']); //je récupère la valeur que je veux ajouter
-  try
-  {
-    // On se connecte à MySQL
-    $bd = new PDO('mysql:host=localhost;dbname=todo_list;charset=utf8', 'root', 'user');
-  }
-  catch(Exception $e)
-  {
-    // En cas d'erreur, on affiche un message et on arrête tout
-    die('Erreur : '.$e->getMessage());
-  }
-  //Export vers la db
 
+  //Export vers la db
   $a_faire= $bd->query("INSERT INTO todolist(id, A_FAIRE, STATUT) VALUES (null,'".$add_tache."','N')");
   //afficher les données de la bd
-  //echo
 }
-if (isset($_POST['button'])  and isset($_POST['list']) and isset($bd)) {//Si j'enregistre et que la db existe et que j'ai coché...
-  $fait= $bd->query("UPDATE todolist SET STATUT='F' WHERE STATUT= 'N'");//Le statut passe de N à F...
-  $fait= $bd->query("SELECT A_FAIRE FROM todolist WHERE statut='F'");//Je reprend les tâches avec F...
+//transformer les tâches en "fait"
+if (isset($_POST['check'])  and isset($_POST['list'])) {//Si j'enregistre et que la db existe et que j'ai coché...
+  for ($i = 0 ; $i < count($_POST['list']); $i++){
+  $bd->query("UPDATE todolist SET STATUT='F' WHERE id='".$_POST['list'][$i]."'");
+  }//Le statut passe de N à F...
 }
+$todo=$bd->query("SELECT A_FAIRE, id FROM todolist WHERE STATUT='N'");
+$todo_done=$bd->query("SELECT A_FAIRE FROM todolist WHERE STATUT='F'");//Je reprend les tâches avec F.
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,17 +57,15 @@ if (isset($_POST['button'])  and isset($_POST['list']) and isset($bd)) {//Si j'e
       <h2>A faire</h2>
       <form action="index.php" method="post" name="formafaire">
         <?php
-        if (isset($bd)) {//si la bd existe et que l'on a ajouté une tâche...
-          $a_faire= $bd->query("SELECT A_FAIRE FROM todolist WHERE statut='N'");
-          while ($donnees = $a_faire->fetch())
-          {
-            echo "<input type='checkbox' name='list[]' value='".$donnees ['A_FAIRE']."'/>
-            <label for='choix'>".$donnees ['A_FAIRE']."</label><br />"; // injecter input.//;
-          }
-          $a_faire->closeCursor();
+        //Afficher ce qui est à faire
+        while ($donnees = $todo->fetch())
+        {
+          echo "<input type='checkbox' name='list[]' value='".$donnees ['id']."'/>
+          <label for='choix'>".$donnees ['A_FAIRE']."</label><br />"; // injecter input.//;
         }
+
         ?>
-        <input type="submit" name="button" value="check" >
+        <input type="submit" name="check" value="check" >
       </form>
     </section>
     <section class="archive">
@@ -73,14 +73,13 @@ if (isset($_POST['button'])  and isset($_POST['list']) and isset($bd)) {//Si j'e
       <form action="index.php" method="post" name="formchecked">
         <div class="done">
           <?php
-          if (isset ($_POST['button']) and !empty($_POST['tache']) and isset($bd)) {//Si j'enregistre et que la db existe et que j'ai coché...
 
-            while ($donnees=$fait ->fetch())
-            {
-              echo "<input type='checkbox' style='text-decoration:line-through, red;' name='list[]' value='".$donnees['A_FAIRE']."'checked/>
-              <label for='choix'>".$donnees['A_FAIRE']."</label><br />";
-            }
+          while ($archive=$todo_done ->fetch())
+          {
+            echo "<input type='checkbox' style='text-decoration:line-through, red;' name='list_done[]' value='".$archive['A_FAIRE']."'checked/>
+            <label for='choix'>".$archive['A_FAIRE']."</label><br />";
           }
+
           ?>
         </form>
       </div>
@@ -96,4 +95,8 @@ if (isset($_POST['button'])  and isset($_POST['list']) and isset($bd)) {//Si j'e
     </footer>
   </div>
 </body>
+<?php
+$todo->closeCursor();
+$todo_done->closeCursor();
+?>
 </html>

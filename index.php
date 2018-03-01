66e8df2d4406a7affebf058ecab9c4641ade7ee3
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-03-01 11:54:20 +0100

    bonus

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //variables date
 $date=$_POST['date'];
-$date_todo= date("d-m-Y");
-echo $date_todo;
+$date_todo= date("Y-m-d");
+
 //sanitization
 function sanitize($key, $filter=FILTER_SANITIZE_STRING){
   $sanitized_variable = null;
@@ -34,18 +34,19 @@ if (isset($_POST['ajouter'])  and !empty ($_POST['tache'])){ //Si on appuie sur 
   $a_faire= $bd->query("INSERT INTO todolist(id, A_FAIRE, STATUT, ECHEANCE) VALUES (null,'".$add_tache."','N','".$date."')");
   //afficher les données de la bd
 }
+//supprimer de la bd. On supprime avant de réécrire dans la bd
+if (isset($_POST['delete'])) {//Si j'appuie sur le bouton delete
+  $delete=$bd->query("DELETE  FROM `todolist`");//Je supprime les données de la table
+}
 //transformer les tâches en "fait"
 if (isset($_POST['check'])  and isset($_POST['list'])) {//Si j'enregistre et que la db existe et que j'ai coché...
   for ($i = 0 ; $i < count($_POST['list']); $i++){
     $bd->query("UPDATE todolist SET STATUT='F' WHERE id='".$_POST['list'][$i]."'");
   }//Le statut passe de N à F...
 }
-$todo=$bd->query("SELECT A_FAIRE, id FROM todolist WHERE STATUT='N'");
+$todo=$bd->query("SELECT A_FAIRE, id,ECHEANCE FROM todolist WHERE STATUT='N'");//Je sélectionne les tâches à faire
 $todo_done=$bd->query("SELECT A_FAIRE FROM todolist WHERE STATUT='F'");//Je reprend les tâches avec F.
 //Supprimer les données dans la table
-if (isset($_POST['delete'])) {//Si j'appuie sur le bouton delete
-  $delete=$bd->query("DELETE  FROM `todolist`");//Je supprime les données de la table
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -67,10 +68,14 @@ if (isset($_POST['delete'])) {//Si j'appuie sur le bouton delete
         //Afficher ce qui est à faire
         while ($donnees = $todo->fetch())
         {
-          echo "<input type='checkbox' name='list[]' value='".$donnees ['id']."'/>
-          <label for='choix'>".$donnees ['A_FAIRE']."</label><br />"; // injecter input.//;
+          if ( $donnees ['ECHEANCE']<=$date_todo) {//Si j'appuie sur ajouter et qu'il y'a une date...
+            echo "<input type='checkbox' name='list[]' value='".$donnees ['id']."'/>
+            <label style='color:red;' for='choix'>".$donnees ['A_FAIRE']."</label><br />"; // injecter input en rouge
+          }else {
+            echo "<input type='checkbox' name='list[]' value='".$donnees ['id']."'/>
+            <label for='choix'>".$donnees ['A_FAIRE']."</label><br />"; // injecter input
+          }
         }
-
         ?>
         <input type="submit" name="check" value="check" >
       </form>
@@ -80,14 +85,12 @@ if (isset($_POST['delete'])) {//Si j'appuie sur le bouton delete
       <form action="index.php" method="post" name="formchecked">
         <div class="done">
           <?php
-
           while ($archive=$todo_done ->fetch())
           {
             echo "<input type='checkbox' style='text-decoration:line-through, red;' name='list_done[]' value='".$archive['A_FAIRE']."'checked/>
             <label for='choix'>".$archive['A_FAIRE']."</label><br />";
           }
           ?>
-
         </form>
       </div>
     </section>
